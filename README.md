343d84814de6753579d2d9cef25d6539843d9145
ZhouQiqi <qiqi.zhou@gmail.com>
2015-09-21 23:27:48 +0800

    第一步：建立一个空项目

--- README.md
+++ README.md
@@ -4,3 +4,26 @@
 
 将会建立多个分支阐述创建一个todo list的过程。
 
+第一步：
+
+### 新建一个空项目
+```
+mkdir todos
+slush yo
+cd client
+spm install
+npm install
+cd server
+npm install
+```
+在mac或者ubuntu上需要用`sudo`。
+
+运行空项目：
+```
+redis-server -port 7777
+
+node app
+```
+可以看到如图：
+![运行成功](http://7mj4k1.com1.z0.glb.clouddn.com/0416BFD2-66BD-4DAA-9246-33ED700FDCE1.png)
+
---
+++ server/stub/routers.js
@@ -0,0 +1,26 @@
+var stub = {
+    '/stub': {
+        data: {
+            words: 'hello world!'
+        },
+        module: 'default'
+    }
+}
+
+module.exports = function(app) {
+    var route;
+    for (route in stub) {
+        var func = procFunc(stub[route]);
+        app.get(route, func);
+    }
+}
+
+function procFunc(proc) {
+    if (typeof proc === 'function') {
+        return proc;
+    } else {
+        return function(req, res) {
+            res.send(proc);
+        }
+    }
+}
